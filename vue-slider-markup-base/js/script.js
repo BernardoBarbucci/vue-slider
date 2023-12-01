3130d9050378bedf251b0806c9f69cf8c1d4b5b2
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-12-01 17:22:50 +0100

    cambiata punteggiatura problematica su js

--- vue-slider-markup-base/js/script.js
+++ vue-slider-markup-base/js/script.js
@@ -54,9 +54,9 @@ const app = Vue.createApp({
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
-            ];
+            ],
             currentIndex: 0
         };
     },
-    
+
 })
